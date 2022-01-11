b6d57047afbf3ffdb8eed74d2e3bed74dea09226
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-11 19:34:58 +0500

    js

--- script.js
+++ script.js
@@ -1537,3 +1537,22 @@ console.log(diff_months(dt1, dt2));
 
 
 
+
+// Write a JavaScript function to get the week start date.  
+
+function startOfWeek(date)
+  {
+    var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+  
+    return new Date(date.setDate(diff));
+ 
+  }
+
+dt = new Date(); 
+
+console.log(startOfWeek(dt).toString());
+
+
+
+
+
