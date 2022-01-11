3ce16184a6ba01d2c72d92546dfadc547e904541
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-11 19:35:10 +0500

    jds

--- script.js
+++ script.js
@@ -1556,3 +1556,23 @@ console.log(startOfWeek(dt).toString());
 
 
 
+
+
+// Write a JavaScript function to get the week end date.  
+
+function endOfWeek(date)
+  {
+     
+    var lastday = date.getDate() - (date.getDay() - 1) + 6;
+    return new Date(date.setDate(lastday));
+ 
+  }
+
+dt = new Date(); 
+
+console.log(endOfWeek(dt).toString());
+
+
+
+
+
